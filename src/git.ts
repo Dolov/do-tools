f33b13119c740dd87b2a370cf9e6a0faf32e4b51
shisongyan <songyan_shi@163.com>
2021-02-10 14:09:37 +0800

    feat: git

---
+++ src/git.ts
@@ -0,0 +1,70 @@
+/**
+ * @fileoverview 封装常用的 git 操作
+ */
+
+const { spawnSync } = require('child_process');
+
+/**
+ * 检查工作区状态
+ */
+const gitWorkspaceStatus = (): string | null => {
+  /** 检查工作区状态 */
+const workspaceStatus = spawnSync('git', ['status', '--porcelain'])
+
+  /** 工作区存在数据 */
+  if (workspaceStatus.stdout.length) {
+    return workspaceStatus.stdout.toString()
+  }
+  return null
+}
+
+/**
+ * 检查暂存区状态
+ */
+const getStageStatus = (): string | null => {
+  const stageStatus = spawnSync('git', ['cherry', '-v'])
+  if (stageStatus.stdout.length) {
+    return stageStatus.stdout.toString()
+  }
+  return null
+}
+
+
+/**
+ * 获取最后的提交信息
+ */
+const getLastCommitMessage = (): {
+  date: string;
+  author: string;
+  commit: string;
+} | null => {
+  const gitLastLog = spawnSync('git', ['log', '-1'])
+  if (gitLastLog.stdout.length) {
+    const log =  gitLastLog.stdout.toString()
+    /** 格式化信息 */
+    return {
+      commit: (log.match(/commit\s*(.+)/) || [])[1],
+      author: (log.match(/Author:\s*(.+)/) || [])[1],
+      date: (log.match(/Date:\s*(.+)/) || [])[1],
+    }
+  }
+  return null
+}
+
+/**
+ * 获取当前分支
+ */
+const getCurrentBranch = (): string | null => {
+  const branch = spawnSync('git', ['rev-parse', '--abbrev-ref', 'HEAD'])
+  if (branch.stdout.length) {
+    return branch.stdout.toString()
+  }
+  return null
+}
+
+export {
+  getStageStatus,
+  gitWorkspaceStatus,
+  getCurrentBranch,
+  getLastCommitMessage,
+}
