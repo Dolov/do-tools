d2af47d932eacc6b7b77b928c79dd7138623c762
shisongyan <songyan_shi@163.com>
2021-09-26 09:16:53 +0800

    feat: 异步改同步

--- src/getDirFilesContent.ts
+++ src/getDirFilesContent.ts
@@ -14,37 +14,26 @@ export interface CallbackParams {
  * @param callback 读取到每个文件后的回调
  */
 const getDirFileContent = (dirPath: string, callback: (file: CallbackParams) => void) => {
-	fs.readdir(dirPath, (err, files) => {
-		if (err) {
-			console.warn(err)
+	const files = fs.readdirSync(dirPath, { encoding: 'utf-8' })
+	files.forEach((fileName: string) => {
+		const filePath = path.join(dirPath, fileName)
+		const stats = fs.statSync(filePath)
+		/** 文件 */
+		const isFile = stats.isFile()
+		/** 文件夹 */
+		const isDir = stats.isDirectory()
+		if (isFile) {
+			const content = fs.readFileSync(filePath, 'utf-8')
+			callback({
+				filePath,
+				file: content,
+				fileName: fileName,
+			})
 			return
 		}
-
-		files.forEach((fileName: string) => {
-			const filePath = path.join(dirPath, fileName)
-			fs.stat(filePath, (err, stats) => {
-				if (err) {
-					console.warn(err)
-					return
-				}
-				/** 文件 */
-				const isFile = stats.isFile()
-				/** 文件夹 */
-				const isDir = stats.isDirectory()
-				if (isFile) {
-					const content = fs.readFileSync(filePath, 'utf-8')
-					callback({
-						filePath,
-						file: content,
-						fileName: fileName,
-					})
-					return
-				}
-				if (isDir) {
-					getDirFileContent(filePath, callback)
-				}
-			})
-		})
+		if (isDir) {
+			getDirFileContent(filePath, callback)
+		}
 	})
 }
 
