1693b4c31f03359311998f9641eb0345f2b60105
石松岩.前端架构组 <songyan_shi@163.com>
2023-08-10 10:57:15 +0800

    filename

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,4 @@ export * as Git from './git'
 export { default as getDirFilesContent } from './getDirFilesContent'
 export * as Reg from './reg'
 
-export { download, downloadWithLog } from './dowload'
+export { download, downloadWithLog } from './download'
