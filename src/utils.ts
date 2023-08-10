5de9c4ca59cac4a91f8041f9c0464c775498b49e
石松岩.前端架构组 <songyan_shi@163.com>
2023-08-10 18:14:36 +0800

    feat: export

--- src/utils.ts
+++ src/utils.ts
@@ -10,3 +10,40 @@ export const generateRandomId = (length: number) => {
   }
   return id;
 }
+
+/** 获取代码字符串中导出的变量 */
+export const filterExported = (code: string) => {
+  const exportRegex = /export\s+(?:default\s+)?(const|let|var|function)\s+(\w+)/g;
+  const commentRegex = /\/\/.*|\/\*[\s\S]*?\*\//g;
+  
+  const exportedCode: string[] = [];
+  let match;
+
+  while ((match = exportRegex.exec(code)) !== null) {
+    const [_, type, name] = match;
+    if (!isCommented(match.index)) {
+      exportedCode.push(name);
+    }
+  }
+
+  return exportedCode;
+
+  function isCommented(index: number) {
+    commentRegex.lastIndex = 0;
+    let isCommented = false;
+    let commentMatch;
+
+    while ((commentMatch = commentRegex.exec(code)) !== null) {
+      if (commentMatch.index > index) {
+        break;
+      }
+
+      if (index <= commentRegex.lastIndex) {
+        isCommented = true;
+        break;
+      }
+    }
+
+    return isCommented;
+  }
+}
