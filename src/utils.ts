a16e89873059681f8f2c48fec867b0be7366d4e2
石松岩.前端架构组 <songyan_shi@163.com>
2023-08-23 10:46:25 +0800

    feat: flattenTree

--- src/utils.ts
+++ src/utils.ts
@@ -3,7 +3,7 @@
 export const generateRandomId = (length: number) => {
   const chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
   let id = '';
-  
+
   for (let i = 0; i < length; i++) {
     let randomIndex = Math.floor(Math.random() * chars.length);
     id += chars[randomIndex];
@@ -15,7 +15,7 @@ export const generateRandomId = (length: number) => {
 export const filterExported = (code: string) => {
   const exportRegex = /export\s+(?:default\s+)?(const|let|var|function)\s+(\w+)/g;
   const commentRegex = /\/\/.*|\/\*[\s\S]*?\*\//g;
-  
+
   const exportedCode: string[] = [];
   let match;
 
@@ -46,4 +46,35 @@ export const filterExported = (code: string) => {
 
     return isCommented;
   }
+}
+
+interface NodeProps {
+  children?: NodeProps[]
+  [key: string]: any
+}
+
+/**
+ * 将对象树结构展开
+ * @param tree 
+ * @returns 
+ */
+export const flattenTree = (tree: NodeProps[]) => {
+  const result: NodeProps[] = [];
+
+  const traverse = (node: NodeProps) => {
+    result.push(node);
+    const { children } = node
+    if (!Array.isArray(children)) return
+    if (children.length > 0) {
+      children.forEach(child => {
+        traverse(child);
+      });
+    }
+  }
+
+  tree.forEach(node => {
+    traverse(node);
+  });
+
+  return result;
 }
